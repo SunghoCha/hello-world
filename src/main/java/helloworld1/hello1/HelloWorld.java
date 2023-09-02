58cd0f071e985876201833dfa60ed60df0b5eb50
SungHo Cha <tjdgh3725@gmail.com>
2023-09-02 19:05:04 +0900

    test3

--- src/main/java/helloworld1/hello1/HelloWorld.java
+++ src/main/java/helloworld1/hello1/HelloWorld.java
@@ -3,6 +3,6 @@ package helloworld1.hello1;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println(" = ");
+        System.out.println(" 1 ");
     }
 }
