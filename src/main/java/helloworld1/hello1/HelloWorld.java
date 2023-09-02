5c4ef444071a88135ccb9032dc299673c02ab4ef
SungHo Cha <tjdgh3725@gmail.com>
2023-09-02 21:06:24 +0900

    구구단 2,3단 구현 - 계산 및 출력

--- src/main/java/helloworld1/hello1/HelloWorld.java
+++ src/main/java/helloworld1/hello1/HelloWorld.java
@@ -2,7 +2,13 @@ package helloworld1.hello1;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println(" 1 ");
+
+        for (int i = 2; i <= 3; i++) {
+            System.out.println(i + "단");
+            for (int j = 1; j < 10; j++) {
+                System.out.println(i + " x " + j + " = " + i * j);
+            }
+            System.out.println();
+        }
     }
 }
