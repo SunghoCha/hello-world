7d94244ca6d1d275beca3e878822b553866dc860
SungHo Cha <tjdgh3725@gmail.com>
2023-09-03 00:13:13 +0900

    메소드 활용해 구구단 다시 구현

--- src/main/java/Gugudan/GugudanV1.java
+++ src/main/java/Gugudan/GugudanV1.java
@@ -1,26 +1,27 @@
 package Gugudan;
 
-import java.io.BufferedReader;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class GugudanV1 {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int[] ints = new int[9];
-
-        int i = Integer.parseInt(br.readLine());
-
-        System.out.println(i + "단");
-        for (int j = 1; j < 10; j++) {
-            ints[j-1] = i * j;
+        for (int i = 2; i < 10; i++) {
+            int[] ints = CalculGugudan(i);
+            printGugudan(ints);
+            System.out.println();
         }
 
+    }
+
+    public static int[] CalculGugudan(int k) {
+        int[] ints = new int[9];
         for (int j = 1; j < 10; j++) {
-            System.out.println(i + " x " + j + " = " + ints[j-1]);
+            ints[j - 1] = k * j;
+        }
+        return ints;
+    }
+
+    public static void printGugudan(int[] a) {
+        for (int j = 1; j < 10; j++) {
+            System.out.println(a[j-1]);
         }
     }
 }
